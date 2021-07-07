fe4381f2cc41f912ed8fde660353c8bed68a9e4b
injung1008 <dlswjd5656@gmail.com>
2021-07-07 19:44:43 +0900

    first commit

---
+++ Study/keras/keras01_2_homework.py
@@ -0,0 +1,21 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+x = np.array([1,2,3,4,5])
+y = np.array([1,2,4,3,5])
+x_pred = [6] #6에 대한 예측값 
+
+model = Sequential()
+model.add(Dense(1, input_dim=1))
+
+model.compile(loss='mse', optimizer='adam')
+
+model.fit(x,y, epochs=16000, batch_size=1)
+
+loss = model.evaluate(x,y)
+print('loss : ', loss)
+
+result = model.predict(x_pred)
+print('x_pred 의 예측값 : ', result)
+
